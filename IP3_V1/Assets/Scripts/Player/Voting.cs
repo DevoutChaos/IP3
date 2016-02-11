143b86191d27e6a932fbf36a0657aca2126539a8
Liam Faulds <chaosend50@gmail.com>
2016-02-11 23:59:40 +0000

    Cautious Optimism
    Due to it being late at night and me wanting bed this has not been
    tested yet (but can't really break anything, so it's all good)
    Updated Voting.cs
    -Should now take inputs and be smart enough to not take multiple inputs
    from one guy.
    -Needs to reset without closing and reopening
    -Needs to be able to verify 3 and 4 player ties better, without
    defaulting to the player who's controller is lowest numerically.

--- IP3_V1/Assets/Scripts/Player/Voting.cs
+++ IP3_V1/Assets/Scripts/Player/Voting.cs
@@ -1,25 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Voting : MonoBehaviour {
+public class Voting : MonoBehaviour
+{
 
     //Declarations
     public bool IsVoting;
     int noOfPlayers;
-    string P1Vote;
-    string P2Vote;
-    string P3Vote;
-    string P4Vote;
+    int P1Vote;
+    int P2Vote;
+    int P3Vote;
+    int P4Vote;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    bool p1Voted;
+    bool p2Voted;
+    bool p3Voted;
+    bool p4Voted;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         Vote();
-	}
+    }
 
     public void Vote()
     {
@@ -36,7 +44,190 @@ public class Voting : MonoBehaviour {
             //A = 0, B = 1, X = 2, Y = 3
             //A = P1, B = P2, X = P3, Y = P4
 
+            //Handle Player 1 Input
+            if (!p1Voted)
+            {
+                if (Input.GetButtonUp("P1voteP1"))
+                {
+                    P1Vote++;
+                    p1Voted = true;
+                }
+                if (Input.GetButtonUp("P1voteP2") && noOfPlayers > 1)
+                {
+                    P2Vote++;
+                    p1Voted = true;
+                }
+                if (Input.GetButtonUp("P1voteP3") && noOfPlayers > 2)
+                {
+                    P3Vote++;
+                    p1Voted = true;
+                }
+                if (Input.GetButtonUp("P1voteP4") && noOfPlayers > 3)
+                {
+                    P4Vote++;
+                    p1Voted = true;
+                }
+            }
+            if (noOfPlayers > 1)
+            {
+                if (!p2Voted)
+                {
+                    if (Input.GetButtonUp("P2voteP1"))
+                    {
+                        P1Vote++;
+                        p2Voted = true;
+                    }
+                    if (Input.GetButtonUp("P2voteP2"))
+                    {
+                        P2Vote++;
+                        p2Voted = true;
+                    }
+                    if (Input.GetButtonUp("P2voteP3") && noOfPlayers > 2)
+                    {
+                        P3Vote++;
+                        p2Voted = true;
+                    }
+                    if (Input.GetButtonUp("P2voteP4") && noOfPlayers > 3)
+                    {
+                        P4Vote++;
+                        p2Voted = true;
+                    }
+                }
+                if (noOfPlayers > 2)
+                {
+                    if (!p3Voted)
+                    {
+                        if (Input.GetButtonUp("P3voteP1"))
+                        {
+                            P1Vote++;
+                            p3Voted = true;
+                        }
+                        if (Input.GetButtonUp("P3voteP2"))
+                        {
+                            P2Vote++;
+                            p3Voted = true;
+                        }
+                        if (Input.GetButtonUp("P3voteP3"))
+                        {
+                            P3Vote++;
+                            p3Voted = true;
+                        }
+                        if (Input.GetButtonUp("P3voteP4") && noOfPlayers > 3)
+                        {
+                            P4Vote++;
+                            p3Voted = true;
+                        }
+                    }
+                    if (noOfPlayers > 3)
+                    {
+                        if (!p4Voted)
+                        {
+                            if (Input.GetButtonUp("P4voteP1"))
+                            {
+                                P1Vote++;
+                                p4Voted = true;
+                            }
+                            if (Input.GetButtonUp("P4voteP2"))
+                            {
+                                P2Vote++;
+                                p4Voted = true;
+                            }
+                            if (Input.GetButtonUp("P4voteP3"))
+                            {
+                                P3Vote++;
+                                p4Voted = true;
+                            }
+                            if (Input.GetButtonUp("P4voteP4"))
+                            {
+                                P4Vote++;
+                                p4Voted = true;
+                            }
+                        }
+                    }
+                }
+            }
+            switch (noOfPlayers)
+            {
+                case 1:
+                    if (p1Voted)
+                    {
+                        Debug.Log("Player 1 voted");
+                    }
+                    break;
 
+                case 2:
+                    if (p1Voted && p2Voted)
+                    {
+                        if (P1Vote > P2Vote)
+                        {
+                            Debug.Log("Player 1 voted");
+                        }
+                        else if (P2Vote > P1Vote)
+                        {
+                            Debug.Log("Player 2 voted");
+                        }
+                        else
+                        {
+                            Debug.Log("Tie for Players 1 and 2");
+                        }
+                    }
+                    break;
+
+                case 3:
+                    if (p1Voted && p2Voted && p3Voted)
+                    {
+                        //***KNOWN ISSUE - IN EVENT OF A DRAW, WILL ALWAYS RETURN PLAYER WHO IS LOWEST NUMERICALLY. ***//
+                        //***COULD FIX BY USING [&& votedC3 !=] ***// 
+                        int votedC3 = Mathf.Max(P1Vote, P2Vote, P3Vote);
+                        if (votedC3 == P1Vote)
+                        {
+                            Debug.Log("Player 1 voted");
+                        }
+                        else if (votedC3 == P2Vote)
+                        {
+                            Debug.Log("Player 2 voted");
+                        }
+                        else if (votedC3 == P3Vote)
+                        {
+                            Debug.Log("Player 3 voted");
+                        }
+                        else
+                        {
+                            Debug.Log("Tie");
+                        }
+
+                    }
+                    break;
+
+                case 4:
+                    if (p1Voted && p2Voted && p3Voted && p4Voted)
+                    {
+                        //***KNOWN ISSUE - IN EVENT OF A DRAW, WILL ALWAYS RETURN PLAYER WHO IS LOWEST NUMERICALLY. ***//
+                        //***COULD FIX BY USING [&& votedC4 !=] ***// 
+                        int votedC4 = Mathf.Max(P1Vote, P2Vote, P3Vote, P4Vote);
+                        if (votedC4 == P1Vote)
+                        {
+                            Debug.Log("Player 1 voted");
+                        }
+                        else if (votedC4 == P2Vote)
+                        {
+                            Debug.Log("Player 2 voted");
+                        }
+                        else if (votedC4 == P3Vote)
+                        {
+                            Debug.Log("Player 3 voted");
+                        }
+                        else if (votedC4 == P4Vote)
+                        {
+                            Debug.Log("Player 4 voted");
+                        }
+                        else
+                        {
+                            Debug.Log("Tie");
+                        }
+                    }
+                    break;
+            }
         }
     }
 }
