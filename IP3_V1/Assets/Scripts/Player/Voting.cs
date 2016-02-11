cbec4fa42adcdc8b5fcc355b834450bf9ded7529
Liam Faulds <chaosend50@gmail.com>
2016-02-11 23:27:15 +0000

    Setup for voting system
    Added new scene to test voting.
    Added Voting.cs
    -Currently counts number of players.
    Updated project settings to allow for 4 players to vote

---
+++ IP3_V1/Assets/Scripts/Player/Voting.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Voting : MonoBehaviour {
+
+    //Declarations
+    public bool IsVoting;
+    int noOfPlayers;
+    string P1Vote;
+    string P2Vote;
+    string P3Vote;
+    string P4Vote;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vote();
+	}
+
+    public void Vote()
+    {
+        if (!IsVoting)
+        {
+            IsVoting = true;
+        }
+        else if (IsVoting)
+        {
+            //Check apparent number of players.
+            noOfPlayers = Input.GetJoystickNames().Length;
+            Debug.Log("Players present: " + noOfPlayers);
+
+            //A = 0, B = 1, X = 2, Y = 3
+            //A = P1, B = P2, X = P3, Y = P4
+
+
+        }
+    }
+}
