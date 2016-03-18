4b75cf39c98aba6dd61760e4699d2675f32aedbd
Liam Faulds <chaosend50@gmail.com>
2016-03-18 10:20:30 +0000

    Voting!!!
    Voting.cs now handles ties appropriately.
    -Voted player (chosen player from tie) is removed from the game.

--- IP3_V1/Assets/Scripts/Player/PlayerController.cs
+++ IP3_V1/Assets/Scripts/Player/PlayerController.cs
@@ -7,55 +7,53 @@ public class PlayerController : MonoBehaviour
 
     //General Declarations
     public float speed = 1f;
-    public float angle1;
-
     public Text scoreText;
     private int score;
+    public Voting voting;
 
     // Use this for initialization
     void Start()
     {
         score = 0;
         SetScoreText();
+
+        //Player1 = GameObject.FindGameObjectWithTag("Player1");
+        //Player2 = GameObject.FindGameObjectWithTag("Player2");
+        //Player3 = GameObject.FindGameObjectWithTag("Player3");
+        //Player4 = GameObject.FindGameObjectWithTag("Player4");
+    }
+
+    void Update()
+    {
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
-        if (this.gameObject.tag == "Player1")
+
+        if (this.gameObject.tag == "Player1" && voting.P1Enabled)
         {
             float horMove1 = Input.GetAxis("Horizontal_P1");
             float verMove1 = Input.GetAxis("Vertical_P1");
-            //Debug.DrawLine(transform.position, followThing1.transform.position, Color.red);
             GetComponent<Rigidbody>().velocity = new Vector3(horMove1 * speed, 0, verMove1 * speed);
-
-            //if (horMove1 != 0 && verMove1 != 0)
-            //{
-            //    angle1 = Mathf.Atan2(horMove1, verMove1) * Mathf.Rad2Deg;
-            //    transform.rotation = Quaternion.Euler(0, angle1, 0);
-            //    //transform.position = Vector3.MoveTowards(transform.position, followThing1.transform.position, speed);
-            //    //GetComponent<Rigidbody>().velocity = Vector3.MoveTowards(transform.position, followThing1.transform.position, speed);
-            //    //GetComponent<Rigidbody>().velocity = (transform.forward * speed);
-            //}
-            
         }
 
-        if (this.gameObject.tag == "Player2")
+        if (this.gameObject.tag == "Player2" && voting.P2Enabled)
         {
             float horMove2 = Input.GetAxis("Horizontal_P2");
             float verMove2 = Input.GetAxis("Vertical_P2");
             GetComponent<Rigidbody>().velocity = new Vector3(horMove2 * speed, 0, verMove2 * speed);
         }
 
-        if (this.gameObject.tag == "Player3")
+        if (this.gameObject.tag == "Player3" && voting.P3Enabled)
         {
             float horMove3 = Input.GetAxis("Horizontal_P3");
             float verMove3 = Input.GetAxis("Vertical_P3");
             GetComponent<Rigidbody>().velocity = new Vector3(horMove3 * speed, 0, verMove3 * speed);
         }
 
-        if (this.gameObject.tag == "Player4")
+        if (this.gameObject.tag == "Player4" && voting.P4Enabled)
         {
             float horMove4 = Input.GetAxis("Horizontal_P4");
             float verMove4 = Input.GetAxis("Vertical_P4");
@@ -65,14 +63,14 @@ public class PlayerController : MonoBehaviour
     }
 
     void OnTriggerEnter(Collider other)
-     {
-         if (other.gameObject.CompareTag("Food"))
-         {
-             other.gameObject.SetActive(false);
-             score++;
-             SetScoreText();
-         }
-     }
+    {
+        if (other.gameObject.CompareTag("Food"))
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            SetScoreText();
+        }
+    }
     void SetScoreText()
     {
         scoreText.text = "Score: " + score.ToString();
--- IP3_V1/Assets/Scripts/Player/Voting.cs
+++ IP3_V1/Assets/Scripts/Player/Voting.cs
@@ -6,16 +6,26 @@ public class Voting : MonoBehaviour
 
     //Declarations
     public bool IsVoting;
-    int noOfPlayers;
-    int P1Vote;
-    int P2Vote;
-    int P3Vote;
-    int P4Vote;
+    public int noOfPlayers;
+    public int P1Vote;
+    public int P2Vote;
+    public int P3Vote;
+    public int P4Vote;
 
     public bool p1Voted;
     public bool p2Voted;
     public bool p3Voted;
     public bool p4Voted;
+
+    public bool P1Enabled = true;
+    public bool P2Enabled = true;
+    public bool P3Enabled = true;
+    public bool P4Enabled = true;
+
+    public GameObject Player1;
+    public GameObject Player2;
+    public GameObject Player3;
+    public GameObject Player4;
 
     // Use this for initialization
     void Start()
@@ -26,23 +36,27 @@ public class Voting : MonoBehaviour
         p4Voted = false;
 
         //Check apparent number of players.
-        noOfPlayers = Input.GetJoystickNames().Length;
+        //noOfPlayers = Input.GetJoystickNames().Length;
+        noOfPlayers = 4;
         Debug.Log("Players present: " + noOfPlayers);
+
+        if (!IsVoting)
+        {
+            IsVoting = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         Vote();
+        PlayerChecks();
     }
 
     public void Vote()
     {
-        if (!IsVoting)
-        {
-            IsVoting = true;
-        }
-        else if (IsVoting)
+
+        if (IsVoting)
         {
             //A = 0, B = 1, X = 2, Y = 3
             //A = P1, B = P2, X = P3, Y = P4
@@ -97,7 +111,6 @@ public class Voting : MonoBehaviour
                         p3Voted = true;
                     }
                 }
-
             }
             if (noOfPlayers > 1)
             {
@@ -164,14 +177,32 @@ public class Voting : MonoBehaviour
                         if (P1Vote > P2Vote)
                         {
                             Debug.Log("Player 1 voted _ Case 2");
+                            P1Enabled = false;
+                            IsVoting = false;
                         }
                         else if (P2Vote > P1Vote)
                         {
                             Debug.Log("Player 2 voted _ Case 2");
+                            P2Enabled = false;
+                            IsVoting = false;
                         }
                         else
                         {
                             Debug.Log("Tie for Players 1 and 2 _ Case 2");
+                            int tieBreak = Random.Range(1, 3);
+                            if (tieBreak == 1)
+                            {
+                                P1Enabled = false;
+                                IsVoting = false;
+                                Debug.Log("Tie Break = " + tieBreak);
+                            }
+                            else if (tieBreak == 2)
+                            {
+                                P2Enabled = false;
+                                IsVoting = false;
+                                Debug.Log("Tie Break = " + tieBreak);
+                            }
+
                         }
                     }
                     break;
@@ -183,14 +214,20 @@ public class Voting : MonoBehaviour
                         if (votedC3 == P1Vote && votedC3 != P2Vote && votedC3 != P3Vote)
                         {
                             Debug.Log("Player 1 voted");
+                            P1Enabled = false;
+                            IsVoting = false;
                         }
                         else if (votedC3 == P2Vote && votedC3 != P1Vote && votedC3 != P3Vote)
                         {
                             Debug.Log("Player 2 voted");
+                            P2Enabled = false;
+                            IsVoting = false;
                         }
                         else if (votedC3 == P3Vote && votedC3 != P1Vote && votedC3 != P2Vote)
                         {
                             Debug.Log("Player 3 voted");
+                            P3Enabled = false;
+                            IsVoting = false;
                         }
                         else
                         {
@@ -199,6 +236,25 @@ public class Voting : MonoBehaviour
                                 case 1:
                                     {
                                         Debug.Log("Tie between P1, P2 and P3");
+                                        int tieBreak = Random.Range(1, 4);
+                                        if (tieBreak == 1)
+                                        {
+                                            P1Enabled = false;
+                                            IsVoting = false;
+                                            Debug.Log("Tie Break = " + tieBreak);
+                                        }
+                                        if (tieBreak == 2)
+                                        {
+                                            P2Enabled = false;
+                                            IsVoting = false;
+                                            Debug.Log("Tie Break = " + tieBreak);
+                                        }
+                                        if (tieBreak == 3)
+                                        {
+                                            P3Enabled = false;
+                                            IsVoting = false;
+                                            Debug.Log("Tie Break = " + tieBreak);
+                                        }
                                         break;
                                     }
                             }
@@ -214,26 +270,59 @@ public class Voting : MonoBehaviour
                         if (votedC4 == P1Vote && votedC4 != P2Vote && votedC4 != P3Vote && votedC4 != P4Vote)
                         {
                             Debug.Log("Player 1 voted");
+                            P1Enabled = false;
+                            IsVoting = false;
                         }
                         else if (votedC4 == P2Vote && votedC4 != P1Vote && votedC4 != P3Vote && votedC4 != P4Vote)
                         {
                             Debug.Log("Player 2 voted");
+                            P2Enabled = false;
+                            IsVoting = false;
                         }
                         else if (votedC4 == P3Vote && votedC4 != P1Vote && votedC4 != P2Vote && votedC4 != P4Vote)
                         {
                             Debug.Log("Player 3 voted");
+                            P3Enabled = false;
+                            IsVoting = false;
                         }
                         else if (votedC4 == P4Vote && votedC4 != P1Vote && votedC4 != P2Vote && votedC4 != P3Vote)
                         {
                             Debug.Log("Player 4 voted");
+                            P4Enabled = false;
+                            IsVoting = false;
                         }
                         else
                         {
-                            switch(votedC4)
+                            switch (votedC4)
                             {
                                 case 1:
                                     {
                                         Debug.Log("Tie between P1, P2, P3 and P4");
+                                        int tieBreak = Random.Range(1, 5);
+                                        if (tieBreak == 1)
+                                        {
+                                            P1Enabled = false;
+                                            IsVoting = false;
+                                            Debug.Log("Tie Break = " + tieBreak);
+                                        }
+                                        if (tieBreak == 2)
+                                        {
+                                            P2Enabled = false;
+                                            IsVoting = false;
+                                            Debug.Log("Tie Break = " + tieBreak);
+                                        }
+                                        if (tieBreak == 3)
+                                        {
+                                            P3Enabled = false;
+                                            IsVoting = false;
+                                            Debug.Log("Tie Break = " + tieBreak);
+                                        }
+                                        if (tieBreak == 4)
+                                        {
+                                            P4Enabled = false;
+                                            IsVoting = false;
+                                            Debug.Log("Tie Break = " + tieBreak);
+                                        }
                                         break;
                                     }
                                 case 2:
@@ -278,6 +367,55 @@ public class Voting : MonoBehaviour
                                             }
                                         }
                                         Debug.Log("Tie between " + tied1 + ", " + tied2);
+                                        int tieBreak = Random.Range(1, 3);
+                                        if (tieBreak == 1)
+                                        {
+                                            if (tied1 == "P1")
+                                            {
+                                                P1Enabled = false;
+                                                IsVoting = false;
+                                            }
+                                            else if (tied1 == "P2")
+                                            {
+                                                P2Enabled = false;
+                                                IsVoting = false;
+                                            }
+                                            else if (tied1 == "P3")
+                                            {
+                                                P3Enabled = false;
+                                                IsVoting = false;
+                                            }
+                                            else if (tied1 == "P4")
+                                            {
+                                                P4Enabled = false;
+                                                IsVoting = false;
+                                            }
+                                            Debug.Log("Tie Break = " + tieBreak);
+                                        }
+                                        if (tieBreak == 2)
+                                        {
+                                            if (tied2 == "P1")
+                                            {
+                                                P1Enabled = false;
+                                                IsVoting = false;
+                                            }
+                                            else if (tied2 == "P2")
+                                            {
+                                                P2Enabled = false;
+                                                IsVoting = false;
+                                            }
+                                            else if (tied2 == "P3")
+                                            {
+                                                P3Enabled = false;
+                                                IsVoting = false;
+                                            }
+                                            else if (tied2 == "P4")
+                                            {
+                                                P4Enabled = false;
+                                                IsVoting = false;
+                                            }
+                                            Debug.Log("Tie Break = " + tieBreak);
+                                        }
                                         break;
                                     }
                             }
@@ -285,6 +423,50 @@ public class Voting : MonoBehaviour
                     }
                     break;
             }
+        }
+    }
+    public void PlayerChecks()
+    {
+        if (!P1Enabled)
+        {
+            Player1.gameObject.GetComponent<Collider>().enabled = false;
+            Player1.gameObject.GetComponent<Renderer>().enabled = false;
+        }
+        if (!P2Enabled)
+        {
+            Player2.gameObject.GetComponent<Collider>().enabled = false;
+            Player2.gameObject.GetComponent<Renderer>().enabled = false;
+        }
+        if (!P3Enabled)
+        {
+            Player3.gameObject.GetComponent<Collider>().enabled = false;
+            Player3.gameObject.GetComponent<Renderer>().enabled = false;
+        }
+        if (!P4Enabled)
+        {
+            Player4.gameObject.GetComponent<Collider>().enabled = false;
+            Player4.gameObject.GetComponent<Renderer>().enabled = false;
+        }
+
+        if (P1Enabled)
+        {
+            Player1.gameObject.GetComponent<Collider>().enabled = true;
+            Player1.gameObject.GetComponent<Renderer>().enabled = true;
+        }
+        if (P2Enabled)
+        {
+            Player2.gameObject.GetComponent<Collider>().enabled = true;
+            Player2.gameObject.GetComponent<Renderer>().enabled = true;
+        }
+        if (P3Enabled)
+        {
+            Player3.gameObject.GetComponent<Collider>().enabled = true;
+            Player3.gameObject.GetComponent<Renderer>().enabled = true;
+        }
+        if (P4Enabled)
+        {
+            Player4.gameObject.GetComponent<Collider>().enabled = true;
+            Player4.gameObject.GetComponent<Renderer>().enabled = true;
         }
     }
 }
